4f91455e9fde621f19da8a6209bcdda37c1a819e
jesse <jesse@163.com>
2022-08-25 20:29:29 +0800

    version1.0

---
+++ src/main/java/com/jesse/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.jesse.git;
+
+/**
+ * @author jesse
+ * @Description
+ * @create 2022-08-25 20:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
